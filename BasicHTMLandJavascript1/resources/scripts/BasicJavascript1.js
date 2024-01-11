cfc78fb334a565be399f5f16741bd48f629c4383
alcyonray <alcyonray@gmail.com>
2024-01-11 00:14:12 -0800

    Basic HTML and Javascript 1 Assignment

---
+++ BasicHTMLandJavascript1/resources/scripts/BasicJavascript1.js
@@ -0,0 +1,8 @@
+function MyFirstFunction() {
+    var string = "Kiss me, I'm Irish!";
+    var result = string.fontcolor("green");
+    document.getElementById("Irish").innerHTML = result;
+}
+function MySecondFunction() {
+    alert("Hello, cruel world...");
+}
